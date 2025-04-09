7ac1c801a6383fce148032d1e1360b83de12a45d
ryutizmwork <izumi@kuroholdgs.jp>
2025-04-09 17:52:45 +0900

    ⚡️ テンプレ構造の変更

--- vite.config.ts
+++ vite.config.ts
@@ -9,42 +9,15 @@ const noAttr = () => {
     }
   }
 }
-const root = 'src'
+
 export default defineConfig({
-  root: root,
-  publicDir: '../public/',
   plugins: [noAttr()],
-  base: './',
+  base: './', // 相対パスで解決
   build: {
-    outDir: '../dist', // ビルド成果物の生成先
     rollupOptions: {
-      output: {
-        // entry chunk assets それぞれの書き出し名の指定
-        entryFileNames: `assets/js/script.js`,
-        chunkFileNames: `assets/js/script.js`,
-        assetFileNames: (assetInfo) => {
-          const { name } = assetInfo
-          if (!name) return 'assets/[name].[ext]'
-          const isImg = /\.( gif|jpeg|jpg|png|svg|webp| )$/.test(name)
-          const isCss = /\.css$/.test(name)
-          let assetType: 'img' | 'css' | undefined = isImg
-            ? 'img'
-            : isCss
-              ? 'css'
-              : undefined
-          switch (assetType) {
-            case 'img':
-              return 'assets/images/[name].[ext]'
-            case 'css':
-              return 'assets/css/style.css'
-            default:
-              return 'assets/[name].[ext]'
-          }
-        }
-      },
       input: {
-        main: resolve(__dirname, root, 'index.html'),
-        about: resolve(__dirname, root, 'about/index.html')
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about/index.html'),
       }
     }
   },
